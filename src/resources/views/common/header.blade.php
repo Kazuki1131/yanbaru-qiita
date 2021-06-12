d6b68027d1c5cc25e435dff975b8be54eac58c1d
minami0601 <minami.love.bb@icloud.com>
2021-06-12 12:08:53 +0900

    ＠authを＠guestに変更

--- src/resources/views/common/header.blade.php
+++ src/resources/views/common/header.blade.php
@@ -6,7 +6,7 @@
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ml-auto">
-        @auth
+        @guest
           <li class="nav-item">
             <a class="nav-link" href="{{ route('login') }}">ログイン</a>
           </li>
