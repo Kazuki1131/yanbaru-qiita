a4c9e738636cabec3b89940a6bcd48084a3afc35
minami0601 <minami.love.bb@icloud.com>
2021-06-17 22:07:44 +0900

    ユーザー新規登録機能追加（諸石）

--- src/app/Http/Controllers/Auth/RegisterController.php
+++ src/app/Http/Controllers/Auth/RegisterController.php
@@ -52,7 +52,11 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password' => ['required', 'string', 'min:8', 'confirmed', 'regex:/^[a-zA-Z0-9]+$/'],
+            'term' => ['required', 'integer', 'digits_between:1,2'],
+        ],
+        [
+            'password.regex' => ':attributeは半角英数字で入力してください。',
         ]);
     }
 
@@ -68,6 +72,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'term' => $data['term'],
         ]);
     }
 }
