e8ec1ef6730cdc38c4ef60a747ac437b89bbfc89
github kazuki1131 <onetsubyo@gmail.com>
2021-06-17 23:45:03 +0900

    記事一覧画面修正

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -11,86 +11,122 @@
     <div class="row">
         <div class="col-md-6">
             <div class="my-4">
-            <div class="card mx-auto w-75">
-                <div class="card-header p-2">
-                    <span class="fas fa-user-edit m-2 font-weight-bold"></span>
-                    <p class="d-inline font-weight-bold">名前</p>
-                    <!-- 編集と削除ボタンはログイン時のみ表示させる -->
-                    <span class="float-right">
-                        <a href="#" class="btn btn-secondary rounded-pill">
-                            <span class="far fa-edit mr-1"></span>編集
-                        </a>
-                        <a href="#" class="btn btn-danger rounded-pill">
-                            <span class="far fa-trash-alt mr-1"></span>削除
-                        </a>
-                    </span>
+                <div class="card mx-auto w-100">
+                    <div class="card-header p-2 h4">
+                        <i class="fas fa-user-edit m-2 font-weight-bold"></i>
+                        <p class="d-inline font-weight-bold">名前</p>
+                        <!-- 編集と削除ボタンはログイン時のみ表示させる -->
+                        <span class="float-right">
+                            <a href="#" class="btn btn-secondary rounded-pill">
+                                <i class="far fa-edit mr-1"></i>編集
+                            </a>
+                            <a href="#" class="btn btn-danger rounded-pill">
+                                <i class="far fa-trash-alt mr-1"></i>削除
+                            </a>
+                        </span>
+                    </div>
+                    <div class="card-body h5">
+                        <dl class="row mb-4">
+                            <dt class="col-4 text-right">期生</dt>
+                            <dd class="col-8">◯期生</dd>
+                        </dl>
+                        <dl class="row mb-4">
+                            <dt class="col-4 text-right">タイトル</dt>
+                            <dd class="col-8">あああああああああああああああああああ</dd>
+                        </dl>
+                        <dl class="row mb-4">
+                            <dt class="col-4 text-right">URL</dt>
+                            <dd class="col-8"><a href="https://qiita.com/">https://qiita.com/</a></dd>
+                        </dl>
+                        <dl class="row mb-4">
+                            <dt class="col-md-4 text-right">投稿日時</dt>
+                            <dd class="col-md-8">{{ NOW() }}</dd>
+                        </dl>
+                        <a href="#" class="btn btn-success d-block w-50 mx-auto">詳細を見る</a>
+                    </div>
                 </div>
-                <div class="card-body ">
-                    <p><p class="d-inline ml-4">期生</p><p class="d-inline ml-4">◯期生</p></p>
-                    <p><p class="d-inline ml-4">タイトル</p><p class="d-inline ml-4">あああああああああああああああ</p></p>
-                    <p><p class="d-inline ml-4">URL</p><a href="#" class="ml-4">link</a></p>
-                    <p><p class="d-inline ml-4">投稿日時</p><p class="d-inline ml-4">{{ NOW() }}</p></p>
-                    <a href="#" class="btn btn-success d-block w-50 mx-auto">詳細を見る</a>
-                </div>
-            </div> 
             </div>
         </div>
         <div class="col-md-6">
             <div class="my-4">
-            <div class="card mx-auto w-75">
-                <div class="card-header p-2">
-                    <span class="fas fa-user-edit m-2 font-weight-bold"></span>
-                    <p class="d-inline font-weight-bold">名前</p>
-                    <!-- 編集と削除ボタンはログイン時のみ表示させる -->
-                    <span class="float-right">
-                        <a href="#" class="btn btn-secondary rounded-pill">
-                            <span class="far fa-edit mr-1"></span>編集
-                        </a>
-                        <a href="#" class="btn btn-danger rounded-pill">
-                            <span class="far fa-trash-alt mr-1"></span>削除
-                        </a>
-                    </span>
+                <div class="card mx-auto w-100">
+                    <div class="card-header p-2 h4">
+                        <i class="fas fa-user-edit m-2 font-weight-bold"></i>
+                        <p class="d-inline font-weight-bold">名前</p>
+                        <!-- 編集と削除ボタンはログイン時のみ表示させる -->
+                        <span class="float-right">
+                            <a href="#" class="btn btn-secondary rounded-pill">
+                                <i class="far fa-edit mr-1"></i>編集
+                            </a>
+                            <a href="#" class="btn btn-danger rounded-pill">
+                                <i class="far fa-trash-alt mr-1"></i>削除
+                            </a>
+                        </span>
+                    </div>
+                    <div class="card-body h5">
+                        <dl class="row mb-4">
+                            <dt class="col-md-4 text-right">期生</dt>
+                            <dd class="col-md-8">◯期生</dd>
+                        </dl>
+                        <dl class="row mb-4">
+                            <dt class="col-md-4 text-right">タイトル</dt>
+                            <dd class="col-md-8">あああああああああああああああああああ</dd>
+                        </dl>
+                        <dl class="row mb-4">
+                            <dt class="col-md-4 text-right">URL</dt>
+                            <dd class="col-md-8"><a href="https://qiita.com/">https://qiita.com/</a></dd>
+                        </dl>
+                        <dl class="row mb-4">
+                            <dt class="col-md-4 text-right">投稿日時</dt>
+                            <dd class="col-md-8">{{ NOW() }}</dd>
+                        </dl>
+                        <a href="#" class="btn btn-success d-block w-50 mx-auto">詳細を見る</a>
+                    </div>
                 </div>
-                <div class="card-body ">
-                    <p><p class="d-inline ml-4">期生</p><p class="d-inline ml-4">◯期生</p></p>
-                    <p><p class="d-inline ml-4">タイトル</p><p class="d-inline ml-4">あああああああああああああああ</p></p>
-                    <p><p class="d-inline ml-4">URL</p><a href="#" class="ml-4">link</a></p>
-                    <p><p class="d-inline ml-4">投稿日時</p><p class="d-inline ml-4">{{ NOW() }}</p></p>
-                    <a href="#" class="btn btn-success d-block w-50 mx-auto">詳細を見る</a>
-                </div>
-            </div>
             </div>
         </div>
     </div>
     <div class="row">
         <div class="col-md-6">
             <div class="my-4">
-            <div class="card mx-auto w-75">
-                <div class="card-header p-2">
-                    <span class="fas fa-user-edit m-2 font-weight-bold"></span>
-                    <p class="d-inline font-weight-bold">名前</p>
-                    <!-- 編集と削除ボタンはログイン時のみ表示させる -->
-                    <span class="float-right">
-                        <a href="#" class="btn btn-secondary rounded-pill">
-                            <span class="far fa-edit mr-1"></span>編集
-                        </a>
-                        <a href="#" class="btn btn-danger rounded-pill">
-                            <span class="far fa-trash-alt mr-1"></span>削除
-                        </a>
-                    </span>
+                <div class="card mx-auto w-100">
+                    <div class="card-header p-2 h4">
+                        <i class="fas fa-user-edit m-2 font-weight-bold"></i>
+                        <p class="d-inline font-weight-bold">名前</p>
+                        <!-- 編集と削除ボタンはログイン時のみ表示させる -->
+                        <span class="float-right">
+                            <a href="#" class="btn btn-secondary rounded-pill">
+                                <i class="far fa-edit mr-1"></i>編集
+                            </a>
+                            <a href="#" class="btn btn-danger rounded-pill">
+                                <i class="far fa-trash-alt mr-1"></i>削除
+                            </a>
+                        </span>
+                    </div>
+                    <div class="card-body h5">
+                        <dl class="row mb-4">
+                            <dt class="col-4 text-right">期生</dt>
+                            <dd class="col-8">◯期生</dd>
+                        </dl>
+                        <dl class="row mb-4">
+                            <dt class="col-4 text-right">タイトル</dt>
+                            <dd class="col-8">あああああああああああああああああああ</dd>
+                        </dl>
+                        <dl class="row mb-4">
+                            <dt class="col-4 text-right">URL</dt>
+                            <dd class="col-8"><a href="https://qiita.com/">https://qiita.com/</a></dd>
+                        </dl>
+                        <dl class="row mb-4">
+                            <dt class="col-md-4 text-right">投稿日時</dt>
+                            <dd class="col-md-8">{{ NOW() }}</dd>
+                        </dl>
+                        <a href="#" class="btn btn-success d-block w-50 mx-auto">詳細を見る</a>
+                    </div>
                 </div>
-                <div class="card-body ">
-                    <p><p class="d-inline ml-4">期生</p><p class="d-inline ml-4">◯期生</p></p>
-                    <p><p class="d-inline ml-4">タイトル</p><p class="d-inline ml-4">あああああああああああああああ</p></p>
-                    <p><p class="d-inline ml-4">URL</p><a href="#" class="ml-4">link</a></p>
-                    <p><p class="d-inline ml-4">投稿日時</p><p class="d-inline ml-4">{{ NOW() }}</p></p>
-                    <a href="#" class="btn btn-success d-block w-50 mx-auto">詳細を見る</a>
-                </div>
-            </div> 
             </div>
         </div>
         <div class="col-md-6">
-            <div class="mt-4">
+            <div class="my-4">
             </div>
         </div>
     </div>
