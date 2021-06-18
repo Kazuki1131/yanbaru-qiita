3f054792bb25678e024bb2cb22b7a399ac14cbdd
github kazuki1131 <onetsubyo@gmail.com>
2021-06-18 11:41:05 +0900

    welcomeからindexにルートを変更

--- src/routes/web.php
+++ src/routes/web.php
@@ -12,9 +12,13 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/articles/create', function () {
+    return view('articles/create');
+});
