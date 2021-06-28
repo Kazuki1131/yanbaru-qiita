b9403b2a17e529c54056a1dc74d21bbc1d7dbff5
github kazuki1131 <onetsubyo@gmail.com>
2021-06-28 22:56:54 +0900

    コントローラーにviewを呼び出すメソッド追加

--- src/app/Http/Controllers/ArticlesController.php
+++ src/app/Http/Controllers/ArticlesController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class ArticlesController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        return view('articles.create');
+    }
 }
