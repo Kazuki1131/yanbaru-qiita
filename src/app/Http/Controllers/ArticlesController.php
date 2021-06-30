258d93d96d8914296a6b25960304ca106d4cbdc7
github kazuki1131 <onetsubyo@gmail.com>
2021-06-30 23:08:18 +0900

    ログインしてなくても記事投稿できるように修正

--- src/app/Http/Controllers/ArticlesController.php
+++ src/app/Http/Controllers/ArticlesController.php
@@ -6,12 +6,8 @@ use Illuminate\Http\Request;
 
 class ArticlesController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
 
-    public function index()
+    public function create()
     {
         return view('articles.create');
     }
