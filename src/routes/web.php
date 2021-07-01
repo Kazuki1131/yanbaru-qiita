8b672866b7567e3fac79ec80c9bc54c2100b10ca
github kazuki1131 <onetsubyo@gmail.com>
2021-07-01 21:31:45 +0900

    nameの部分を修正

--- src/routes/web.php
+++ src/routes/web.php
@@ -19,4 +19,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/articles/create', 'ArticlesController@create')->name('article');
+Route::get('/articles/create', 'ArticlesController@create')->name('articles.create');
