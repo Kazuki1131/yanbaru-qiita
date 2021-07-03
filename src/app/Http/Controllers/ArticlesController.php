938b420786342dbf3250ce9252a2fc37262eb8da
github kazuki1131 <onetsubyo@gmail.com>
2021-07-03 21:05:18 +0900

    記事一覧画面作成(かずき)

--- src/app/Http/Controllers/ArticlesController.php
+++ src/app/Http/Controllers/ArticlesController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Article;
 
 class ArticlesController extends Controller
 {
+    public function index()
+    {
+        $articles = Article::orderBy('created_at', 'desc')->paginate(5);
+        return view('articles.index', compact('articles'));
+    } 
 
     public function create()
     {
