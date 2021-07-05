adc658f9222461b3d86216ad28debe33f0a05922
haruki0016 <haruki.ukiuki@gmail.com>
2021-07-05 22:33:38 +0900

    マイページ画面の実装

--- src/app/Http/Controllers/UsersController.php
+++ src/app/Http/Controllers/UsersController.php
@@ -4,12 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use App\Article;
+use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
     public function show()
     {
-        //マイページ画面
-        return view('user.show');
+        //ユーザーデータ取得
+        $auth = Auth::user();
+        $articles = Article::where('user_id', $auth->id)->get();
+
+        return view('user.show', ['auth' => $auth, 'articles' => $articles]);
     }
 }
--- src/resources/views/user/show.blade.php
+++ src/resources/views/user/show.blade.php
@@ -14,15 +14,15 @@
         <div class="card-body pt-2 pb-5">
             <dl class="row justify-content-center">
                 <dt>名前</dt>
-                <dd>はるき</dd>
+                <dd>{{ $auth->name }}</dd>
             </dl>
             <dl class="row justify-content-center">
                 <dt>期生</dt>
-                <dd>8期生</dd>
+                <dd>{{ $auth->term }}期生</dd>
             </dl>
             <dl class="row justify-content-center">
                 <dt>メールアドレス</dt>
-                <dd>@@@@@@</dd>
+                <dd>{{ $auth->email }}</dd>
             </dl>
             <div class="row justify-content-center">
                 <a class="px-4 btn btn-secondary" href="#">戻る</a>
@@ -31,10 +31,12 @@
         </div>
     </div>
     <h3 div class="row justify-content-center mt-5">自分の投稿</h3>
+
+    @foreach ($articles as $article)
     <div class="card user-show w-50 mx-auto mt-5">
         <div class="card-header">
             <div class="d-flex align-items-center">
-                <div><i class="fas fa-user-edit mr-3"></i>投稿日時 : 2021-06-26</div>
+                <div><i class="fas fa-user-edit mr-3"></i>投稿日時 : {{ $article->created_at }}</div>
                 <div class="ml-auto btn-box01">
                     <a href="#" class="px-2 btn btn-secondary">編集</a>
                     <a href="#" class="px-2 btn btn-danger">削除</a>
@@ -44,41 +46,18 @@
         <div class="card-body">
             <dl class="row justify-content-center">
                 <dt>タイトル</dt>
-                <dd>Laravel</dd>
+                <dd>{{ $article->title }}</dd>
             </dl>
             <dl class="row justify-content-center">
                 <dt>URL</dt>
-                <dd><a href="https://qiita.com/shimotaroo/items/6a909797e0139517b1bd">https://qiita.com/shimotaroo/items/6a909797e0139517b1bd</a></dd>
+                <dd><a href="{{ $article->url }}" target="_blank" rel="noopener">{{ $article->url }}</a></dd>
             </dl>
             <div class="row justify-content-center">
                 <a class="px-5 btn btn-success" href="#">詳細を見る</a>
             </div>
         </div>
     </div>
-    <div class="card user-show w-50 mx-auto mt-5">
-        <div class="card-header">
-            <div class="d-flex align-items-center">
-                <div><i class="fas fa-user-edit mr-3"></i>投稿日時 : 2021-06-26</div>
-                <div class="ml-auto btn-box01">
-                    <a href="#" class="px-2 btn btn-secondary">編集</a>
-                    <a href="#" class="px-2 btn btn-danger">削除</a>
-                </div>
-            </div>
-        </div>
-        <div class="card-body">
-            <dl class="row justify-content-center">
-                <dt>タイトル</dt>
-                <dd>Laravel</dd>
-            </dl>
-            <dl class="row justify-content-center">
-                <dt>URL</dt>
-                <dd><a href="https://qiita.com/shimotaroo/items/6a909797e0139517b1bd">https://qiita.com/shimotaroo/items/6a909797e0139517b1bd</a></dd>
-            </dl>
-            <div class="row justify-content-center">
-                <a class="px-5 btn btn-success" href="#">詳細を見る</a>
-            </div>
-        </div>
-    </div>
+    @endforeach
 </div>
 
 @endsection
